74dc21e81779a6f593112228697fccb8efd59ee7
Md. Kobirul Islam <kobirul05j@gmail.com>
2025-05-25 23:11:51 +0600

    solve 2 problem and add data for solving pormlem

---
+++ wildlife.sql
@@ -0,0 +1,54 @@
+CREATE DATABASE conservation_db;
+
+CREATE Table rangers(
+    ranger_id SERIAL PRIMARY KEY,
+    "name" VARCHAR(50) NOT NULL,
+    region VARCHAR(100)
+);
+
+INSERT INTO rangers (ranger_id, "name", region)
+VALUES
+(1, 'Alice Green', 'Northern Hills'),
+(2, 'Bob White', 'River Delta'),
+(3, 'Carol King', 'Mountain Range');
+
+
+CREATE TABLE species (
+    species_id SERIAL PRIMARY KEY,
+    common_name VARCHAR(100) NOT NULL,
+    scientific_name VARCHAR(150) NOT NULL,
+    discovery_date DATE NOT NULL,
+    conservation_status VARCHAR(50) NOT NULL
+);
+
+INSERT INTO species (species_id, common_name, scientific_name, discovery_date, conservation_status)
+VALUES
+(1, 'Snow Leopard', 'Panthera uncia', '1775-01-01', 'Endangered'),
+(2, 'Bengal Tiger', 'Panthera tigris tigris', '1758-01-01', 'Endangered'),
+(3, 'Red Panda', 'Ailurus fulgens', '1825-01-01', 'Vulnerable'),
+(4, 'Asiatic Elephant', 'Elephas maximus indicus', '1758-01-01', 'Endangered');
+
+
+CREATE TABLE sightings (
+    sighting_id SERIAL PRIMARY KEY,
+    ranger_id INT NOT NULL REFERENCES rangers(ranger_id),
+    species_id INT NOT NULL REFERENCES species(species_id),
+    sighting_time TIMESTAMP NOT NULL,
+    location VARCHAR(255) NOT NULL,
+    notes TEXT
+);
+
+INSERT INTO sightings (sighting_id, species_id, ranger_id, location, sighting_time, notes)
+VALUES
+(1, 1, 1, 'Peak Ridge', '2024-05-10 07:45:00', 'Camera trap image captured'),
+(2, 2, 2, 'Bankwood Area', '2024-05-12 16:20:00', 'Juvenile seen'),
+(3, 3, 3, 'Bamboo Grove East', '2024-05-15 09:10:00', 'Feeding observed'),
+(4, 1, 2, 'Snowfall Pass', '2024-05-18 18:30:00', NULL);
+
+-- Problem -1
+INSERT INTO rangers(ranger_id, "name", region) 
+VALUES (4, 'Derek Fox', 'Coastal Plains');
+
+-- Problem 2
+SELECT COUNT (DISTINCT species_id) AS unique_Species_count 
+FROM sightings;
